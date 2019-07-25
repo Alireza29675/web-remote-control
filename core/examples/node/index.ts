5263c2f247c6eb047b28f212f002880af08e29e3
Alireza Sheikholmolouki <alireza.sheikholmolouki@gmail.com>
2019-07-25 19:30:46 +0430

    chore: better server debugging

--- core/examples/node/index.ts
+++ core/examples/node/index.ts
@@ -1,3 +1,9 @@
+import chalk from 'chalk'
+import clear from 'clear'
+import moment from 'moment'
 import Server from '../../src/server'
+
+clear()
+console.log(chalk`{green Server re-started at} {yellow [${moment().format(`mm:ss`)}]}`)
 
 const server = new Server()
